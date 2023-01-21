0dae6ce7ffbb96bb2081881b87e9916f838c5611
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-01-21 00:39:02 -0500

    added function for creating LI's

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,13 +20,50 @@ function startTimer() {
     }
   }, 1000);
 }
-
+// define start button in HTML
 let startButton = document.getElementById("start-button")
-
-startButton.addEventListener("click",startQuiz)
-
+// defin the question component in HTML
+let questionComponent = document.getElementById("question-component")
+// define Element with the questions
 let preQuizElement = document.getElementById("pre-quiz")
 
+// add the listener to start quiz button to start function
+startButton.addEventListener("click",startQuiz)
+
+const questionList = ['Commonly used data types DO NOT include:'];
+
+const answerList = ['strings', 'Booleans', 'alerts', 'numbers'];
+const answerList2 = ['test', 'test', 'test', 'test']
+
 function startQuiz (){
-  preQuizElement.classList.add("hidden")
+  preQuizElement.classList.add("hidden");
+  questionComponent.classList.remove("hidden");
+  question.textContent += "Commonly used data types do not include";
+  
+  
+  answerList.forEach((array) => {
+    let li = document.createElement('li');
+    li.innerText = array;
+    possibleAnswers.appendChild(li);
+  });
+  
+  setTimeout(wait, 3000)
+
 }
+
+let question = document.getElementById('question-text')
+let possibleAnswers = document.getElementById('answer-list')
+
+
+
+function wait(){
+  document.getElementById('answer-list').innerHTML = '';
+
+  answerList2.forEach((array) => {
+    let li = document.createElement('li');
+    li.innerText = array;
+    possibleAnswers.appendChild(li);
+  });
+}
+
+// let buttonChange = document.append('button')
