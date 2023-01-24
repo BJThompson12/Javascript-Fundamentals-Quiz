ff9056a8b3eb5ae7c280ef4cfcb02c1cf7e5e724
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-01-24 09:57:33 -0500

    fixed question button styling and lalighnment

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -184,7 +184,7 @@ function setTime () {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = '0:' + secondsLeft;
+    timeEl.textContent = secondsLeft;
   
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
